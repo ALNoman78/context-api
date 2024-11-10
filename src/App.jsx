606e0952d0748446dfd21b2e821467d390d826d6
ALNoman78 <abdullaalno078@gmail.com>
2024-11-10 19:38:12 +0600

    price section added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>Abdullah Al Noman</h1>
+      <button className='btn btn-outline btn-primary'>Clicked</button>
+    </>
+  )
+}
+
+export default App
